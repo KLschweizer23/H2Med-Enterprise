fa42aa5ad85a7f68409cc9ea62ea788da72269b2
Ken <schweizer23ss@gmail.com>
2021-11-06 14:02:42 +0800

    Added SalesObject

--- src/inventorysystem/PurchasesObject.java
+++ src/inventorysystem/PurchasesObject.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package inventorysystem;
 
 /**
@@ -10,5 +6,49 @@ package inventorysystem;
  * @author User
  */
 public class PurchasesObject {
-    
+    private String date;
+    private String supplier;
+    private String referenceNumber;
+    private String collectionReceipt;
+    private String amount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getReferenceNumber() {
+        return referenceNumber;
+    }
+
+    public void setReferenceNumber(String referenceNumber) {
+        this.referenceNumber = referenceNumber;
+    }
+
+    public String getCollectionReceipt() {
+        return collectionReceipt;
+    }
+
+    public void setCollectionReceipt(String collectionReceipt) {
+        this.collectionReceipt = collectionReceipt;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
---
+++ src/inventorysystem/SalesObject.java
@@ -0,0 +1,10 @@
+
+package inventorysystem;
+
+/**
+ *
+ * @author User
+ */
+public class SalesObject {
+    
+}
